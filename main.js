c47b0bd46f869903d47ab31d26f0f467a293aeea
Danny Dingeldein <TheDDingeldein@gmail.com>
2022-04-18 19:55:10 -0500

    test js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("loops-data")
+const button = document.getElementById("main-btn")
+button.innerHTML="Ah, Push it!"
+console.log(button)
+const button2 = document.querySelector("#main-btn")
+console.log(button2)
